de832d4d5d2a7610866ce34ff440b85a4b6f6e00
Phil Aquilina <philaquilina@gmail.com>
2016-07-11 20:33:20 -0700

    correct right ascension/declination range checks

--- night_sky.py
+++ night_sky.py
@@ -1,5 +1,6 @@
 import csv
 import math
+from enum import Enum
 from datetime import datetime
 
 from parse_hipparcos import VISIBLE_STAR_PATH
@@ -8,6 +9,7 @@ J2000 = datetime(year=2000, month=1, day=1, hour=12)
 JDAY_IN_SECONDs = 3600 * 24
 
 MILLBRAE = (37.5985, 237.612)
+HEMI = Enum('Hemisphere', ('northern', 'southern'))
 
 def greenwich_sidereal_time():
     """ Returns the greenwich mean sidereal time in degrees.
@@ -29,21 +31,6 @@ def local_sidereal_time(local_lng_eastward):
     """
     return (greenwich_sidereal_time() + local_lng_eastward) % 360
 
-def _range_lng(lng):
-    MIN = 0
-    MAX = 360
-
-    start_lng = lng - 90
-    end_lng = lng + 90
-
-    if start_lng < MIN:
-        start_lng = MAX + start_lng
-
-    if end_lng > MAX:
-        end_lng = end_lng - MAX
-
-    return [start_lng, end_lng]
-
 def _range_lat(lat):
     MIN = -90
     MAX = 90
@@ -57,26 +44,30 @@ def _range_lat(lat):
     if end_lat > MAX:
         end_lat = MAX - lat
 
-    return [start_lat, end_lat]
+    hemi = HEMI.southern
+    if lat > 0:
+        hemi = HEMI.northern
 
-def _in_ra_range(coord, _range):
-    # handle the case where the range value carried over:
-    # ex: 320° +-90° is a range of [230, 50] since 410 exceeds 360 by 50
-    if _range[0] > _range[1]:
-        return coord > _range[0] or coord < _range[1]
+    return [start_lat, end_lat, hemi]
 
-    return _range[0] < coord < _range[1]
+def _in_ra_range(ra, _range):
+    # to account for ranges like [210, 30], which we represent as [210, 390],
+    # simply add 360 to our right ascension
+    start, end = _range
+    return start < ra < end or start < ra + 360 < end
 
-def _in_dec_range(coord, _range):
-    # TODO
-    pass
-
+def _in_dec_range(dec, _range):
+    # handle the "carry over" from going past 90 degrees
+    if _range[2] == HEMI.northern:
+        return _range[0] < dec < 90 or _range[1] < dec < 90
+    else:
+        return -90 < dec < _range[1] or _range[0] < dec < -90
 
 def current_night_sky(lat, lng):
     lmst = local_sidereal_time(lng)
 
-    range_ra = _range_lng(lng)
-    range_dec = _range_lat(lmst)
+    range_ra = [lmst - 90, lmst + 90]
+    range_dec = _range_lat(lat)
 
     in_night_sky = []
     with open(VISIBLE_STAR_PATH) as f:
