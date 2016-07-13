eb47f817fef5bf16b3f560619c1918e6b07bac20
Phil Aquilina <philaquilina@gmail.com>
2016-07-12 22:12:05 -0700

    update range formulas again

--- night_sky.py
+++ night_sky.py
@@ -1,6 +1,4 @@
 import csv
-import math
-from enum import Enum
 from datetime import datetime
 
 from parse_hipparcos import VISIBLE_STAR_PATH
@@ -9,7 +7,6 @@ J2000 = datetime(year=2000, month=1, day=1, hour=12)
 JDAY_IN_SECONDs = 3600 * 24
 
 MILLBRAE = (37.5985, 237.612)
-HEMI = Enum('Hemisphere', ('northern', 'southern'))
 
 def greenwich_sidereal_time():
     """ Returns the greenwich mean sidereal time in degrees.
@@ -31,37 +28,12 @@ def local_sidereal_time(local_lng_eastward):
     """
     return (greenwich_sidereal_time() + local_lng_eastward) % 360
 
-def _range_lat(lat):
-    MIN = -90
-    MAX = 90
+def in_ra_range(ra, lng, plus_minus):
+    angle_diff = abs(lng - ra) % 360
+    return angle_diff < plus_minus or 360 - angle_diff < plus_minus
 
-    start_lat = lat - 90
-    end_lat = lat + 90
-
-    if start_lat < MIN:
-        start_lat = MIN + lat
-
-    if end_lat > MAX:
-        end_lat = MAX - lat
-
-    hemi = HEMI.southern
-    if lat > 0:
-        hemi = HEMI.northern
-
-    return [start_lat, end_lat, hemi]
-
-def _in_ra_range(ra, _range):
-    # to account for ranges like [210, 30], which we represent as [210, 390],
-    # simply add 360 to our right ascension
-    start, end = _range
-    return start < ra < end or start < ra + 360 < end
-
-def _in_dec_range(dec, _range):
-    # handle the "carry over" from going past 90 degrees
-    if _range[2] == HEMI.northern:
-        return _range[0] < dec < 90 or _range[1] < dec < 90
-    else:
-        return -90 < dec < _range[1] or _range[0] < dec < -90
+def in_dec_range(dec, lat, plus_minus):
+    return abs(lat - dec) < plus_minus
 
 def current_night_sky(lat, lng):
     """ Find all visible stars in the sky above.
@@ -76,9 +48,6 @@ def current_night_sky(lat, lng):
     """
     lmst = local_sidereal_time(lng)
 
-    range_ra = [lmst - 90, lmst + 90]
-    range_dec = _range_lat(lat)
-
     in_night_sky = []
     with open(VISIBLE_STAR_PATH) as f:
         reader = csv.DictReader(f)
@@ -90,7 +59,7 @@ def current_night_sky(lat, lng):
             except ValueError:
                 continue
 
-            if _in_ra_range(star_ra, range_ra) and _in_dec_range(star_dec, range_dec):
+            if in_ra_range(star_ra, lmst, 90) and in_dec_range(star_dec, lat, 90):
                 in_night_sky.append(row)
 
     return in_night_sky
